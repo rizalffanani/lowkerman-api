9429d96edc50f011e2f3b68ff1b441e033d1e77a
rizal <rizalffanani@gmail.com>
2025-01-14 16:35:12 +0700

    update

---
+++ middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization']
+    if (!token) return res.status(401).json({ message: 'Access Denied' })
+
+    try {
+        const verified = jwt.verify(token.split(' ')[1], 'lowkerman_secret_key_api')
+        req.user = verified
+        next()
+    } catch (err) {
+        res.status(403).json({ message: 'Invalid Token' })
+    }
+}
+
+const authorizeRole = (role) => (req, res, next) => {
+    if (req.user.role !== role) {
+        return res.status(403).json({ message: 'Access Denied: Insufficient Permissions' })
+    }
+    next()
+}
+
+module.exports = { authenticateToken, authorizeRole }
